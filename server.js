641ded5febd2040abc4e8570e7dc9a76b8fddec0
Reid Schroder <reidschroder2@gmail.com>
2022-02-27 18:21:54 -0500

    created server.js and route folder for api and html routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+app.use(express.urlencoded({ extend: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
